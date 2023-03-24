748521535fa6169847c8363d96c63f8a778682bf
levietcao27 <95171573+levietcao27@users.noreply.github.com>
2023-03-25 01:35:53 +0700

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -7,13 +7,47 @@
     <title>Quản lý điểm rèn luyện</title>
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="public/css/formSV.css">
+    <link rel="stylesheet" href="Ttdtsgu.css">
 </head>
 <body>
     <div id="main">
-        <header id="header_section">
-            <div class="header-content">
-                <p class="title_header">Quản lý điểm rèn luyện</p>
-            </div>
-        </header>
+        <center>
+           <div class="base" style="height: 210px">
+                <table>
+                    <tbody>
+                        <tr>
+                        <header id="header_section">
+                            <div class="header-content">
+                               <td>
+                                <div>
+                                    <img src="./Bg.png" alt="">
+                                </div>
+                                <div class="menu" style="background-color: rgb(235, 235, 233);">
+                                    <div class="out-menu"><a href="#"><b>TRANG CHỦ</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>ĐĂNG KÝ MÔN HỌC</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>XEM TKB</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>XEM LỊCH THI</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>XEM HỌC PHÍ</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>XEM ĐIỂM</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>XEM CTĐT</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>XEM MÔN TQ</b></a></div>
+                                    <div class="out-menu"><a href=""><b>ĐIỂM RÈN LUYỆN</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>SỬA TT CÁ NHÂN</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>GÓP Ý KIẾN</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>ĐÁNH GIÁ GIẢNG DẠY</b></a></div>
+                                    <div class="out-menu"><a href="#"><b>HÓA ĐƠN ĐIỆN TỬ</b></a></div>
+                                </div>
+                                </td> 
+                            </div>
+                        </header>
+                            
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div> 
+        </center>
+        
+        
        
    
